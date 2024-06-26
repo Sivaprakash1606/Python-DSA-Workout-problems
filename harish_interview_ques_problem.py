cefc3f2f924bb0cd195ffa4fe4d0152e74b64694
Sivaprakash1606 <sivasnprakash3@gmail.com>
2024-06-26 08:51:18 +0530

    harish challenge interview question in some changes

--- harish_interview_ques_problem.py
+++ harish_interview_ques_problem.py
@@ -1,8 +1,9 @@
 string="     Merfantz   Tech    Pvt    Ltd        Welcome     "
+s=string.split()
 spaces=[]
 count=0
-for i in range(len(string)):
-    if string[i] == " ":
+for i in string:
+    if i == " ":
         count+=1
     else:
         if count!=0:
@@ -11,6 +12,9 @@ for i in range(len(string)):
 spaces.append(count)
 n=len(spaces)-2
 print(spaces)
+for i in s:
+    print(i,end=" ")
+print()
 print(f"First space count: {spaces[0]}")
 print(f"Last space count: {spaces[-1]}")
 print(f"Middle space count: {sum(spaces[1:-1])-n}")
