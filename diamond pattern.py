27351e6d78264628ce9982e24a295682e78b9098
Sivaprakash1606 <sivasnprakash3@gmail.com>
2024-08-19 13:38:10 +0530

    diamond pattern

--- diamond pattern.py
+++ diamond pattern.py
@@ -1,12 +1,7 @@
-num=5
-for i in range(0,num):
-    l=num-(num-i//2)
-    r=l+i
-    for j in range(0,num):
-        if j>=l and j<=r:
-            print("*",end=" ")
-        else:
-            print(end="  ")
-    print(" ")
+n=5
+for i in range(0,n):
+    print(" "*(n-i-1) + "*"*(2*i+1))
+for i in range(n-2,-1,-1):
+    print(" "*(n-i-1) + "*"*(2*i+1))
 
 
