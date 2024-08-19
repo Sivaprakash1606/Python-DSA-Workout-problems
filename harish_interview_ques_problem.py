41a7ba18f88ff36242c9febd5d44664ab93610d7
Sivaprakash1606 <sivasnprakash3@gmail.com>
2024-08-19 23:40:50 +0530

    harish interview ques

--- harish_interview_ques_problem.py
+++ harish_interview_ques_problem.py
@@ -1,54 +1,21 @@
-def find_itinerary(tickets):
-    def find(allticket, i,res):
-        # if not res:
-        #     res.append(allticket[i][0])
-        #     res.append(allticket[i][1])
-        #     allticket.pop(i)
-        # else:
+string="     Merfantz   Tech    Pvt    Ltd        Welcome     "
+s=string.split()
+spaces=[]
+count=0
+for i in string:
+    if i == " ":
+        count+=1
+    else:
+        if count!=0:
+            spaces.append(count)
+            count=0
+spaces.append(count)
+n=len(spaces)-2
+print(spaces)
+for i in s:
+    print(i,end=" ")
+print()
+print(f"First space count: {spaces[0]}")
+print(f"Last space count: {spaces[-1]}")
+print(f"Middle space count: {sum(spaces[1:-1])-n}")
 
-        for j in range(len(allticket)):
-            find=False
-            for k in range(len(allticket)):
-                if res[-1]==allticket[k][0]:
-                    res.append(allticket[k][0])
-                    res.append(allticket[k][1])
-                    allticket.pop(k)
-                    find=True
-                    break
-            if find==False:
-                res=[]
-                return False
-        if not allticket:
-            return res
-
-    allticket = tickets[:]
-    res=[]
-    for i in range(len(tickets)):
-        # result = find(allticket, i,res)
-        res.append(allticket[i][0])
-        res.append(allticket[i][1])
-        allticket.pop(i)
-        result = find(allticket, i, res)
-        if result == False:
-            allticket=tickets[:]
-            res=[]
-        else:
-            return res
-
-tickets1 = [
-    ["Madurai", "Coimbatore"],
-    ["Bangalore", "Pondicherry"],
-    ["Pune", "Kolkatta"],
-    ["Coimbatore", "Pune"],
-    ["Kolkatta", "Pondicherry"],
-    ["Pondicherry", "Bangalore"],
-    ["Pondicherry", "Madurai"],
-]
-tickets2 = [
-    ["Chennai", "Banglore"],
-    ["Bombay", "Delhi"],
-    ["Goa", "Chennai"],
-    ["Delhi", "Goa"]
-]
-print(find_itinerary(tickets1))
-print(find_itinerary(tickets2))
